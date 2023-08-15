69b997b64f168f975a56998d9d8d003f4c751959
Zasoba <vladyslav.v.zasoba@mercedes-benz.com>
2023-08-15 22:30:37 +0200

    Use constants for Media Type

--- src/main/java/ua/ithillel/tomcatrs/UserResource.java
+++ src/main/java/ua/ithillel/tomcatrs/UserResource.java
@@ -2,6 +2,7 @@ package ua.ithillel.tomcatrs;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import ua.ithillel.tomcatrs.model.User;
 import ua.ithillel.tomcatrs.service.UserService;
 
@@ -16,7 +17,7 @@ public class UserResource {
 
     // /users
     @GET
-    @Produces(value = "application/json")
+    @Produces(value = MediaType.APPLICATION_JSON)
     public List<User> getAll() {
         return userService.getUsers();
     }
@@ -24,14 +25,14 @@ public class UserResource {
     // /users/12
     @GET
     @Path("/{id}")
-    @Produces(value = "application/json")
+    @Produces(value = MediaType.APPLICATION_JSON)
     public User getById(@PathParam("id") Integer id) {
         return userService.getUserById(id);
     }
 
     // /users
     @POST
-    @Consumes(value = "application/json")
+    @Consumes(value = MediaType.APPLICATION_JSON)
     public void add(User user) {
         userService.addUser(user);
     }
@@ -39,14 +40,15 @@ public class UserResource {
     // /users/12
     @PUT
     @Path("/{id}")
-    @Produces(value = "application/json")
-    @Consumes(value = "application/json")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    @Consumes(value = MediaType.APPLICATION_JSON)
     public User add(@PathParam("id") Integer id, User newUser) {
         return userService.updateUser(id, newUser);
     }
 
     @DELETE
     @Path("/{id}")
+    @Produces(value = MediaType.APPLICATION_JSON)
     public User delete(@PathParam("id") Integer id) {
         return userService.deleteUser(id);
     }
