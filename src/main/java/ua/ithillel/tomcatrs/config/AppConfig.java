8f7a9aae82e4fed3bba838a78527ccbdbc7f383f
Zasoba <vladyslav.v.zasoba@mercedes-benz.com>
2023-08-16 20:06:43 +0200

    Remove unnecessary imports

--- src/main/java/ua/ithillel/tomcatrs/config/AppConfig.java
+++ src/main/java/ua/ithillel/tomcatrs/config/AppConfig.java
@@ -1,9 +1,7 @@
 package ua.ithillel.tomcatrs.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
-import ua.ithillel.tomcatrs.UserResource;
 import ua.ithillel.tomcatrs.binder.MyAppBinder;
-import ua.ithillel.tomcatrs.service.UserServiceDefault;
 
 public class AppConfig extends ResourceConfig {
     public AppConfig() {
