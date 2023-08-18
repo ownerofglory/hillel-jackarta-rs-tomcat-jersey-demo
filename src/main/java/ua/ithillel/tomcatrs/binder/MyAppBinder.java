fc0bf3fe17832f9bbc070b87424e1574192e5b3e
Zasoba <vladyslav.v.zasoba@mercedes-benz.com>
2023-08-18 20:49:46 +0200

    Remove unnecessary config class

--- src/main/java/ua/ithillel/tomcatrs/binder/MyAppBinder.java
+++ src/main/java/ua/ithillel/tomcatrs/binder/MyAppBinder.java
@@ -3,11 +3,16 @@ package ua.ithillel.tomcatrs.binder;
 import jakarta.inject.Singleton;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import ua.ithillel.tomcatrs.service.UserService;
+import ua.ithillel.tomcatrs.service.UserServiceDefault;
 
 public class MyAppBinder extends AbstractBinder {
     @Override
     protected void configure() {
-        bindFactory(new UserServiceFactory())
+//        bindFactory(new UserServiceFactory())
+//                .to(UserService.class)
+//                .in(Singleton.class);
+
+        bind(UserServiceDefault.class)
                 .to(UserService.class)
                 .in(Singleton.class);
     }
